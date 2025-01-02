c3c837588744e165c4e6aa2140f72625ccaf3a88
yusuf sevinchan <yusufsevinchan@ogr.iuc.edu.tr>
2025-01-02 21:10:30 +0300

    Update function

--- netlify/functions/getKeys.js
+++ netlify/functions/getKeys.js
@@ -1,36 +1,51 @@
 export const handler = async (event) => {
-    console.log("Event received:", event);
+  // CORS kontrolü ile sadece belirli domainlerin bu fonksiyonu kullanmasına izin ver
+  const allowedOrigins = ["chrome-extension://gmpkpoccbompnndpfkijbcpgjaenekla"];
+  const origin = event.headers.origin;
 
-    // CORS kontrolü ile sadece belirli domainlerin bu fonksiyonu kullanmasına izin ver
-    const allowedOrigins = ["chrome-extension://gmpkpoccbompnndpfkijbcpgjaenekla"];
-    const origin = event.headers.origin;
-
-    console.log("Origin:", origin);
-
-    if (!allowedOrigins.includes(origin)) {
-        console.log("Origin not allowed:", origin);
-        return {
-            statusCode: 403,
-            body: JSON.stringify({ message: "Erişim engellendi" }),
-        };
-    }
-
-    // CORS kontrolü başarılı ise, keyleri döndür
-    const keys = {
-        GEOAPIFY_API_KEY: process.env.GEOAPIFY_API_KEY,
-        LOCATIONIQ_API_KEY: process.env.LOCATIONIQ_API_KEY,
-        OPENWEATHERMAP_API_KEY: process.env.OPENWEATHERMAP_API_KEY,
-    };
-
-    console.log("Keys:", keys);
-
-    return {
+  // OPTIONS request ise CORS kontrolü yapma
+  if (event.httpMethod === "OPTIONS") {
+    if (allowedOrigins.includes(origin)) {
+      return {
         statusCode: 200,
         headers: {
-            "Access-Control-Allow-Origin": origin,
-            "Access-Control-Allow-Methods": "GET, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type",
+          "Access-Control-Allow-Origin": origin,
+          "Access-Control-Allow-Methods": "GET, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type",
         },
-        body: JSON.stringify(keys),
+        body: JSON.stringify({ message: "CORS başarılı" }),
+      };
+    }
+    else {
+      return {
+        statusCode: 403,
+        body: JSON.stringify({ message: "Cors başarısız" }),
+      };
+    }
+  }
+
+  // CORS kontrolü başarısız ise, erişimi engelle
+  if (!allowedOrigins.includes(origin)) {
+    return {
+      statusCode: 403,
+      body: JSON.stringify({ message: "Erişim engellendi" }),
     };
+  }
+
+  // CORS kontrolü başarılı ise, keyleri döndür
+  const keys = {
+    GEOAPIFY_API_KEY: process.env.GEOAPIFY_API_KEY,
+    LOCATIONIQ_API_KEY: process.env.LOCATIONIQ_API_KEY,
+    OPENWEATHERMAP_API_KEY: process.env.OPENWEATHERMAP_API_KEY,
+  };
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": origin,
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+    body: JSON.stringify(keys),
+  };
 };
