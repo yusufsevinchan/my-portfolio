94610c421d2b0b534f7c75671e044bfd2387b2f0
yusuf sevinchan <yusufsevinchan@ogr.iuc.edu.tr>
2025-01-02 20:49:35 +0300

    Add function

---
+++ netlify/functions/getKeys.js
@@ -0,0 +1,24 @@
+export const handler = async (event) => {
+  // CORS kontrolü ile sadece belirli domainlerin bu fonksiyonu kullanmasına izin ver
+  const allowedOrigins = ["chrome-extension:/gmpkpoccbompnndpfkijbcpgjaenekla"];
+  const origin = event.headers.origin;
+
+  if (!allowedOrigins.includes(origin)) {
+    return {
+      statusCode: 403,
+      body: JSON.stringify({ message: "Erişim engellendi" }),
+    };
+  }
+
+  // CORS kontrolü başarılı ise, keyleri döndür
+  const keys = {
+    geoapifyApiKey: process.env.GEOAPIFY_API_KEY,
+    locationiqApiKey: process.env.LOCATIONIQ_API_KEY,
+    openWeatherApiKey: process.env.OPENWEATHERMAP_API_KEY,
+  };
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(keys),
+  };
+};
