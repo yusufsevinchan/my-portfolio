7b6080abceb752f2280fec78e50f266f04d894ab
Yusuf <yusufsevinchan13@gmail.com>
2025-01-04 10:42:32 +0300

    update function

--- netlify/functions/getKeys.js
+++ netlify/functions/getKeys.js
@@ -1,8 +1,8 @@
 export const handler = async (event) => {
   // CORS kontrolü ile sadece belirli domainlerin bu fonksiyonu kullanmasına izin ver
   const allowedOrigins = [
-    "chrome-extension://gmpkpoccbompnndpfkijbcpgjaenekla",
-    "moz-extension://a6a6cbb7-d4fe-4a97-98f8-3dba534db98e"
+    "chrome-extension://dpnppkohaamjanddepocpipmjmlmobbp/",
+    "moz-extension://a6a6cbb7-d4fe-4a97-98f8-3dba534db98e/"
   ];
   const origin = event.headers.origin;
 
