060228e2ce09ecb65e30a8616d298a9dd74b0d18
Yusuf <yusufsevinchan13@gmail.com>
2025-01-29 01:27:10 +0300

    SEO improvements, performance enhancement, and responsive design updates

--- assets/js/about.js
+++ assets/js/about.js
@@ -8,15 +8,12 @@ function hideProfileOnScroll() {
 
   // If profile photo exists and the device is mobile
   if (profile && window.innerWidth <= 768) {
-    // Add event listener to scroll event
     window.addEventListener("scroll", () => {
       // Get the distance from the top of the page
       const distanceFromTop = window.scrollY;
 
-      // Hide the profile photo when scrolled down
       if (distanceFromTop > 50) {
-        // Change 100 based on your needs
-        profile.style.opacity = 0; // Hide
+        profile.style.opacity = 0;
       } else {
         profile.style.opacity = 1;
       }
--- components/footer/footer.js
+++ components/footer/footer.js
@@ -3,37 +3,51 @@ document.addEventListener("DOMContentLoaded", init);
 async function init() {
   // Fetch footer.html and insert it into the page
   await loadFooterContent();
+
   // Prevent same page reload
   preventSameLinkReload();
 }
 
 async function loadFooterContent() {
-  await fetch("/components/footer/footer.html")
-    .then((response) => response.text())
-    .then((html) => {
-      const footerDiv = document.createElement("div");
-      footerDiv.id = "footer-div";
-      footerDiv.innerHTML = html;
-      // insert the footer element as the last child of the body
-      document.body.appendChild(footerDiv);
-    })
-    .catch((error) => {
-      console.warn(error);
-    });
+  const cachedHtml = localStorage.getItem("footerHtml");
+  if (cachedHtml) {
+    renderFooter(cachedHtml);
+  } else {
+    let html = await fetchFooterContent();
+    if (html) {
+      localStorage.setItem("footerHtml", html);
+      renderFooter(html);
+    }
+  }
+}
+
+function renderFooter(html) {
+  const footerDiv = document.createElement("div");
+  footerDiv.id = "footer-div";
+  footerDiv.innerHTML = html;
+
+  // insert the footer element as the last child of the body
+  document.body.appendChild(footerDiv);
+}
+
+async function fetchFooterContent() {
+  try {
+    const response = await fetch("/components/footer/footer.html");
+    if (!response.ok) throw new Error("Footer yüklenemedi");
+    return await response.text();
+  } catch (error) {
+    console.error("Footer yükleme hatası:", error);
+  }
 }
 
 function preventSameLinkReload() {
-  // tüm <a> etiketleri içinde <li> öğesine tıklama olayını yakala
-  const anchors = document.querySelectorAll("footer a");
-  anchors.forEach(function (anchor) {
-    anchor.addEventListener("click", (event) =>
-      anchorClickHandler(event, anchor)
-    );
+  const footer = document.querySelector("footer");
+  footer.addEventListener("click", (event) => {
+    if (
+      event.target.tagName === "A" &&
+      event.target.href === window.location.href
+    ) {
+      event.preventDefault();
+    }
   });
-}
-
-function anchorClickHandler(event, anchor) {
-  if (anchor.href === window.location.href) {
-    event.preventDefault();
-  }
 }
